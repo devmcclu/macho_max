54dc06cf7f4af17482b8af81bd29e0c4e30be8e4
devmcclu <devmclu@iu.edu>
2018-10-09 16:22:02 -0400

    Create project

---
+++ McClure_SurvivalShooter/Assets/_Complete-Game/Scripts/Enemy/Done_EnemyMovement.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace CompleteProject
+{
+    public class Done_EnemyMovement : MonoBehaviour
+    {
+        Transform player;               // Reference to the player's position.
+        Done_PlayerHealth playerHealth;      // Reference to the player's health.
+        Done_EnemyHealth enemyHealth;        // Reference to this enemy's health.
+        UnityEngine.AI.NavMeshAgent nav;               // Reference to the nav mesh agent.
+
+
+        void Awake ()
+        {
+            // Set up the references.
+            player = GameObject.FindGameObjectWithTag ("Player").transform;
+            playerHealth = player.GetComponent <Done_PlayerHealth> ();
+            enemyHealth = GetComponent <Done_EnemyHealth> ();
+            nav = GetComponent <UnityEngine.AI.NavMeshAgent> ();
+        }
+
+
+        void Update ()
+        {
+            // If the enemy and the player have health left...
+            if(enemyHealth.currentHealth > 0 && playerHealth.currentHealth > 0)
+            {
+                // ... set the destination of the nav mesh agent to the player.
+                nav.SetDestination (player.position);
+            }
+            // Otherwise...
+            else
+            {
+                // ... disable the nav mesh agent.
+                nav.enabled = false;
+            }
+        }
+    }
+}
