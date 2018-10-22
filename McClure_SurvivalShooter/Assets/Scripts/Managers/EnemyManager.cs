461b7ba7e8dfdb264f2da8971ad16eab5295d351
devmcclu <devmcclu@iu.edu>
2018-10-22 17:15:20 -0400

    Create random spawn times for enemy spawners

--- McClure_SurvivalShooter/Assets/Scripts/Managers/EnemyManager.cs
+++ McClure_SurvivalShooter/Assets/Scripts/Managers/EnemyManager.cs
@@ -4,18 +4,22 @@ public class EnemyManager : MonoBehaviour
 {
     public PlayerHealth playerHealth;
     public GameObject enemy;
-    public float spawnTime = 3f;
+    public float spawnTime; // = Random.Range(3.0f, 20.0f);//3f;
     public Transform[] spawnPoints;
+    //float number = Random.Range(3.0f, 20.0f);
 
 
     void Start ()
     {
+        spawnTime = spawnTime = Random.Range(3.0f, 20.0f);
         InvokeRepeating ("Spawn", spawnTime, spawnTime);
     }
 
 
     void Spawn ()
     {
+
+
         if(playerHealth.currentHealth <= 0f)
         {
             return;
